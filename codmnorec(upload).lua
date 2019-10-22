780e54fc970d26daadf74eb99b78f41a273f8a3b
pensiunanhacker <49938328+pensiunanhacker@users.noreply.github.com>
2019-10-23 05:42:24 +0800

    Add files via upload

---
+++ codmnorec(upload).lua
@@ -0,0 +1,242 @@
+--p.e.n.s.i.u.n.a.n.h.a.c.k.e.r
+function Home1()
+nHome = gg.choice (
+{
+--1
+"🔘 norecoil (CODM) \n     [click to active 📂]",
+},
+nil,
+" ")
+if nHome == 1 then norecCODM() end
+--this code bellow just for fix error when "nil"
+if nHome == nil then else EXIT() end
+end
+-----------------------------pastecode room....
+function norecCODM()
+norec1()
+norec2()
+norec3()
+norec4()
+norec5()
+norec6()
+norec7()
+norec8()
+norec9()
+norec10()
+gg.alert("☑Done norecoil all weapon")
+end
+
+function norec1()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1038174126;1038174126;1017370378;1101004800;1092616192;1090519040:57", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local p = gg.getResults(3000)
+for i, v in ipairs(p) do
+if v.flags == gg.TYPE_DWORD then
+v.value = "1"
+v.freeze = true
+end
+end
+gg.addListItems(p)
+p = nil
+gg.processResume()
+gg.clearResults()
+end
+function norec2()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1041194025;1041194025;1008981770;1091567616;1091567616;1076887552:57", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local p = gg.getResults(3000)
+for i, v in ipairs(p) do
+if v.flags == gg.TYPE_DWORD then
+v.value = "1"
+v.freeze = true
+end
+end
+gg.addListItems(p)
+p = nil
+gg.processResume()
+gg.clearResults()
+end
+function norec3()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1038174126;1040187392;1000593162;1101004800;1091567616;1076132577;1073741824:97", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local p = gg.getResults(3000)
+for i, v in ipairs(p) do
+if v.flags == gg.TYPE_DWORD then
+v.value = "1"
+v.freeze = true
+end
+end
+gg.addListItems(p)
+p = nil
+gg.processResume()
+gg.clearResults()
+end
+function norec4()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1038174126;1038174126;1018980991;1097859072;1091567616;1070050836:57", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local p = gg.getResults(3000)
+for i, v in ipairs(p) do
+if v.flags == gg.TYPE_DWORD then
+v.value = "1"
+v.freeze = true
+end
+end
+gg.addListItems(p)
+p = nil
+gg.processResume()
+gg.clearResults()
+end
+function norec5()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1035489772;1036160860;1014350479;1097859072;1.092616192;1070134723:57", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local p = gg.getResults(3000, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(p) do
+if v.flags == gg.TYPE_DWORD then
+v.value = "1"
+v.freeze = true
+end
+end
+gg.addListItems(p)
+p = nil
+gg.processResume()
+gg.clearResults()
+end
+function norec6()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1038845215;1039516303;1011129254;1097859072;1091567616;1069086147;1046562734:81", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local p = gg.getResults(3000)
+for i, v in ipairs(p) do
+if v.flags == gg.TYPE_DWORD then
+v.value = "1"
+v.freeze = true
+end
+end
+gg.addListItems(p)
+p = nil
+gg.processResume()
+gg.clearResults()
+end
+function norec7()
+gg.clearResults()
+gg.searchNumber("1040522936;1040522936;1000593162:9", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local p = gg.getResults(3000)
+for i, v in ipairs(p) do
+if v.flags == gg.TYPE_DWORD then
+v.value = "0"
+v.freeze = true
+end
+end
+gg.addListItems(p)
+p = nil
+gg.processResume()
+gg.clearResults()
+end
+function norec8()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1024416809;1024416809;1024416809;1101004800;1094713344;1089470464:57", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local p = gg.getResults(3000)
+for i, v in ipairs(p) do
+if v.flags == gg.TYPE_DWORD then
+v.value = "1"
+v.freeze = true
+end
+end
+gg.addListItems(p)
+p = nil
+gg.processResume()
+gg.clearResults()
+end
+function norec9()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1032805417;1034147594;1008981770:9", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local p = gg.getResults(3000)
+for i, v in ipairs(p) do
+if v.flags == gg.TYPE_DWORD then
+v.value = "1"
+v.freeze = true
+end
+end
+gg.addListItems(p)
+p = nil
+gg.processResume()
+gg.clearResults()
+end
+function norec10()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1035489772;1035489772;1012202996:9", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local p = gg.getResults(3000, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(p) do
+if v.flags == gg.TYPE_DWORD then
+v.value = "0"
+v.freeze = true
+end
+end
+gg.addListItems(p)
+p = nil
+gg.processResume()
+gg.clearResults()
+end
+-----------------------------------------------------------------------
+--function FOOTER AND EXIT bellow
+function EXIT()
+gg.setVisible(false) --false to hide -- true to view
+--there are 15 memory range on your device
+--added 1
+--green
+gg.setRanges(
+gg.REGION_JAVA_HEAP |
+gg.REGION_C_HEAP |
+gg.REGION_C_ALLOC |
+gg.REGION_C_DATA |
+gg.REGION_C_BSS |
+gg.REGION_PPSSPP |
+gg.REGION_ANONYMOUS |
+--yellow
+gg.REGION_JAVA |
+gg.REGION_STACK |
+gg.REGION_ASHMEM |
+--yellowlight
+gg.REGION_OTHER |
+--red
+gg.REGION_BAD |
+--purple
+gg.REGION_CODE_APP |
+--purple_light
+gg.REGION_CODE_SYS |
+--added
+gg.REGION_VIDEO)
+-----------------------------------------------------------------------
+--function skipRestore for restore...
+--your setting memory saved
+gg.skipRestoreState()
+-----------------------------------------------------------------------
+--code exit from your operating system
+os.exit()
+end 
+-----------------------------------------------------------------------
+--function HOME yout can put it on top too
+function HOME()
+anythingyoucanwritehere=1
+Home1()
+end
+-----------------------------------------------------------------------
+--PH letter is your code menu script
+while(true)do
+if gg.isVisible(true) then PH=1
+gg.setVisible(false)
+end
+-----------------------------------------------------------------------
+if PH==1 then Home1() os.exit() end
+end
+-----------------------------------------------------------------------
+
+
